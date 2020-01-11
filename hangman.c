b514c4c7199bda8ae122915cd5de1577192c52fe
mertfozzy <59746147+mertfozzy@users.noreply.github.com>
2020-01-11 20:41:17 +0300

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -10,18 +10,92 @@ int main(void)
 	char guessWords [] [16] =
 	
 	{
-	"istanbul",
-	"izmir",
-	"tokat",
-	"bolu",
-	"kirsehir"
-	
-	
-	};
+	"adana", 
+"adiyaman", 
+"afyon", 
+"agri", 
+"amasya", 
+"ankara", 
+"antalya", 
+"artvin", 
+"aydin",
+"balikesir", 
+"bilecik", 
+"bingol", 
+"bitlis", 
+"bolu", 
+"burdur", 
+"bursa",
+"canakkale", 
+"cankiri", 
+"corum", 
+"denizli", 
+"diyarbakir", 
+"edirne",
+"elazig",
+"erzincan",
+"erzurum", 
+"eskisehir",
+"gaziantep",
+"giresun", 
+"gumushane",
+"hakkari", 
+"hatay", 
+"isparta",
+"mersin", 
+"istanbul",
+"izmir",
+"kars", 
+"kastamonu", 
+"kayseri", 
+"kirklareli",
+"kirsehir", 
+"kocaeli", 
+"konya",
+"kutahya", 
+"malatya",
+"manisa", 
+"kahramanmaras",
+"mardin",
+"mugla",
+"mus",
+"nevsehir",
+"nigde",
+"ordu",
+"rize",
+"sakarya",
+"samsun",
+"siirt",
+"sinop",
+"sivas", 
+"tekirdag", 
+"tokat",
+"trabzon",
+"tunceli", 
+"sanliurfa", 
+"usak",
+"van", 
+"yozgat",
+"zonguldak",
+"aksaray",
+"bayburt", 
+"karaman",
+"kirikkale", 
+"batman",
+"sirnak",
+"bartin",
+"ardahan",
+"igdir",
+"yalova", 
+"karabuk", 
+"kilis", 
+"osmaniye",
+"duzce"
+};
 	
 	// index for random word
 	
-	int randomIndex = rand() % 5; // assign module into the amount of province you have	
+	int randomIndex = rand() % 81; // assign module into the amount of province you have	
 	int numLives = 5;
 	int numCorrect = 0;
 	int oldCorrect = 0;	
@@ -160,4 +234,3 @@ int main(void)
 	
 	return 0;
 }
-
